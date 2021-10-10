8c970b1fbf91d3ae2329841ffb5f736c7745a1f2
Exe Sanoner <exe.sanoner@gmail.com>
2021-10-10 20:05:16 -0300

    fix: last changes

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
-$(document).ready(function () {
+import $ from "jquery";
+import "./styles.css";
+
+$(function () {
   // CREATE GRID TABLE
   (function createGrid() {
-    const $window = $(window);
-
     const squaresXCount = 100; // number of squares along x-axis
-    const squareSide = ($window.width() - (squaresXCount + 1)) / squaresXCount; // width (and height) of one square
-    const squaresYCount = Math.floor($window.height() / squareSide); // number of squares along y-axis
+    const squareSide =
+      ($(window).width() - (squaresXCount + 1)) / squaresXCount; // width (and height) of one square
+    const squaresYCount = Math.floor($(window).height() / squareSide); // number of squares along y-axis
     const maxGridHeight = squareSide * squaresYCount + squaresYCount + 1;
 
     const gridElem = $(".grid-square");
@@ -13,7 +15,7 @@ $(document).ready(function () {
 
     for (let i = 0; i < squaresYCount; i++) {
       for (let j = 0; j < squaresXCount; j++) {
-        $('<div class="square"></div>').appendTo(gridElem);
+        $('<div class="square" />').appendTo(gridElem);
       }
     }
   })();
@@ -29,48 +31,55 @@ $(document).ready(function () {
   const colorBtn7 = $(".btn7-color");
   const colorBtn8 = $(".btn8-color");
 
-  const defaultColor = "btn8-color";
-
-  let currentColor = null;
-  let IsClickDown = false;
-
-  let initWithDefaultColor = true;
+  let currentColor = "btn8-color";
+  let isClickDown = false;
 
   $(".square").bind({
     mousedown: function (event) {
-      let selected = $(event.target);
-      IsClickDown = true;
-      if (event.which === 1 && initWithDefaultColor === true) {
-        if (!selected.hasClass(defaultColor)) {
-          selected.removeClass(colors).addClass(defaultColor);
-        } else if (selected.hasClass(defaultColor)) {
-          IsClickDown = true;
-          selected.removeClass(defaultColor);
-        }
-      }
+      event.stopImmediatePropagation();
+      isClickDown = true;
     },
-
-    // Paint while holding down the button
-    mouseenter: function (event) {
+    mouseleave: function (event) {
       let selected = $(event.target);
       if (
         event.which === 1 &&
-        initWithDefaultColor === true &&
-        IsClickDown &&
-        !selected.hasClass(defaultColor)
+        isClickDown &&
+        !selected.hasClass(currentColor)
       ) {
-        selected.removeClass(colors).addClass(defaultColor);
+        selected.removeClass(colors).toggleClass(currentColor);
       }
     },
-    mouseup: function () {
-      IsClickDown = false;
+    mouseenter: function (event) {
+      // Paint while holding down the button
+      let selected = $(event.target);
+      if (
+        event.which === 1 &&
+        isClickDown &&
+        !selected.hasClass(currentColor)
+      ) {
+        selected.removeClass(colors).toggleClass(currentColor);
+      }
+    },
+    click: function (event) {
+      isClickDown = false;
+      // Prevents other listeners of the same event from being called
+      // Listeners called in the order in which they were added
+      event.stopImmediatePropagation();
+      let selected = $(event.target);
+      if (event.which === 1) {
+        const hasCurrentColorClass = selected.hasClass(currentColor);
+        selected.removeClass(colors);
+        if (!hasCurrentColorClass) {
+          selected.addClass(currentColor);
+        }
+        isClickDown = true;
+      }
     },
   });
 
   // RIGHT-CLICK EVENTS
   $(document).contextmenu(function (event) {
-    createColorSelector(event);
-    initWithDefaultColor = false;
+    showColorSelector(event);
 
     colorBtn1.on("click", function () {
       currentColor = "btn1-color";
@@ -97,41 +106,6 @@ $(document).ready(function () {
       currentColor = "btn8-color";
     });
 
-    // PAINT SQUARE
-
-    $(".square").bind({
-      mousedown: function (event) {
-        // Prevents other listeners of the same event from being called
-        // Listeners called in the order in which they were added
-        event.stopImmediatePropagation();
-        let selected = $(event.target);
-        IsClickDown = true;
-        if (event.which === 1) {
-          if (!selected.hasClass(currentColor)) {
-            selected.removeClass(colors).addClass(currentColor);
-          } else if (selected.hasClass(currentColor)) {
-            IsClickDown = true;
-            selected.removeClass(colors);
-          }
-        }
-      },
-
-      // Paint while holding down the button
-      mouseenter: function (event) {
-        let selected = $(event.target);
-        if (
-          event.which === 1 &&
-          IsClickDown &&
-          !selected.hasClass(currentColor)
-        ) {
-          selected.removeClass(colors).toggleClass(currentColor);
-        }
-      },
-      mouseup: function () {
-        IsClickDown = false;
-      },
-    });
-
     // Disable browser-specific context menu
     return false;
   });
@@ -146,7 +120,7 @@ $(document).ready(function () {
     $(this).fadeOut(500);
   });
 
-  function createColorSelector(e) {
+  function showColorSelector(e) {
     // Get window size
     const winWidth = $(document).width();
     const winHeight = $(document).height();
@@ -159,6 +133,9 @@ $(document).ready(function () {
     const menuHeight = $("div.color-selector").height();
     // Min margin (minimum distance from the specific edge of the window)
     const minEdgeMargin = -5;
+
+    let menuLeft;
+    let menuTop;
 
     // Detect if the place where the selector label appears exceeds the range of the window
     // First: lower right corner exceeds the window
